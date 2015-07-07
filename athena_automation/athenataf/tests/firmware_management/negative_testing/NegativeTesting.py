0f73ca367191fd6243e87e2f1fa2df621cd1d3bc
raju-tm <raju_set@testmile.com>
2015-07-07 19:18:04 +0530

    TestClass: Updating ScheduleUpgrade and NegativeTesting

--- athena_automation/athenataf/tests/firmware_management/negative_testing/NegativeTesting.py
+++ athena_automation/athenataf/tests/firmware_management/negative_testing/NegativeTesting.py
@@ -3,71 +3,71 @@ logger = logging.getLogger('athenataf')
 from athenataf.lib.functionality.test.AthenaGUITestCase import AthenaGUITestCase
 
 class NegativeTesting(AthenaGUITestCase):
-	'''
-	Test class for Negative Testing.
-	'''
-	
-	def test_ath_4414_cant_initiate_another_upgrade_when_upgrading(self):
-		conf=self.config.config_vars
-		firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
-		firmware_page.select_vc_for_upgrade("IAP_1")
-		firmware_page.clicking_on_upgrade_firmware()
-		firmware_page.setting_firmware_upgrade_manual_option(conf.version_type_value_2,conf.firmware_downgrade_version)
-		firmware_page.click_post_firmware_upgrade()
-		time.sleep(5)
-		firmware_page.select_vc_for_upgrade("IAP_1")
-		firmware_page.assert_device_selector_disabled()
+    '''
+    Test class for Negative Testing.
+    '''
+    
+    def test_ath_4414_cant_initiate_another_upgrade_when_upgrading(self):
+        conf=self.config.config_vars
+        firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
+        firmware_page.select_vc_for_upgrade("IAP_1")
+        firmware_page.clicking_on_upgrade_firmware()
+        firmware_page.setting_firmware_upgrade_manual_option(conf.version_type_value_2,conf.firmware_downgrade_version)
+        firmware_page.click_post_firmware_upgrade()
+        time.sleep(5)
+        firmware_page.select_vc_for_upgrade("IAP_1")
+        firmware_page.assert_device_selector_disabled()
 
-		firmware_page.buy_time()
-		DeviceLibrary.getPrompt("IAP_1")
-		DeviceLibrary.connect_device_to_server("IAP_1")
-		firmware_page.select_vc_for_upgrade("IAP_1")
-		firmware_page.clicking_on_upgrade_firmware()
-		firmware_page.setting_firmware_upgrade_manual_option(option=conf.version_type_value_2,version=conf.firmware_base_version)
-		firmware_page.click_post_firmware_upgrade()
-		firmware_page.buy_time()
-		DeviceLibrary.getPrompt("IAP_1")
-		DeviceLibrary.connect_device_to_server("IAP_1")
-		
-		
-	def test_ath_6758_upgrade_firmware_without_selecting_vc(self):
-		firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
-		firmware_page.upgrade_button.click()
-		firmware_page.assert_no_vc_error_message()
-		firmware_page.ok_button.click()
-		
-	def _view_monitoring_pages(self):
-		self.LeftPanel.go_to_monitoring_page()
-		self.LeftPanel.go_to_monitoring_access_points()
-		self.LeftPanel.go_to_monitoring_clients()
-		self.LeftPanel.go_to_monitoring_wids()
-		self.LeftPanel.go_to_monitoring_event_log()
-		self.LeftPanel. go_to_monitoring_notifications()
+        firmware_page.buy_time()
+        DeviceLibrary.getPrompt("IAP_1")
+        DeviceLibrary.connect_device_to_server("IAP_1")
+        firmware_page.select_vc_for_upgrade("IAP_1")
+        firmware_page.clicking_on_upgrade_firmware()
+        firmware_page.setting_firmware_upgrade_manual_option(option=conf.version_type_value_2,version=conf.firmware_base_version)
+        firmware_page.click_post_firmware_upgrade()
+        firmware_page.buy_time()
+        DeviceLibrary.getPrompt("IAP_1")
+        DeviceLibrary.connect_device_to_server("IAP_1")
+        
+        
+    def test_ath_6758_upgrade_firmware_without_selecting_vc(self):
+        firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
+        firmware_page.upgrade_button.click()
+        firmware_page.assert_no_vc_error_message()
+        firmware_page.ok_button.click()
+        
+    def _view_monitoring_pages(self):
+        self.LeftPanel.go_to_monitoring_page()
+        self.LeftPanel.go_to_monitoring_access_points()
+        self.LeftPanel.go_to_monitoring_clients()
+        self.LeftPanel.go_to_monitoring_wids()
+        self.LeftPanel.go_to_monitoring_event_log()
+        self.LeftPanel. go_to_monitoring_notifications()
 
-	def test_ath_6836_check_ui_stability_after_page_reload(self):
-		self._view_monitoring_pages()
-		self.browser.refresh()
-		firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
-		firmware_page.isPageLoaded()	
-		self.logout_and_login_back()
-		firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
-		firmware_page.isPageLoaded()  
-	
-	def test_ath_8343_downgrade_iap_224_225_to_34_and_blow(self):
-		conf = self.config.config_vars
-		firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
-		firmware_page.upgrade_firmware_using_custom_build_option(conf.version_firmware,"IAP_1")
-		firmware_page.asserting_version_unavailable_img()
-	
-	def test_ath_4426_downgrade_iap_224_225_to_34_and_blow(self):
-		conf = self.config.config_vars
-		firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
-		firmware_page.upgrade_firmware_using_custom_build_option(conf.invalid_version_firmware,"IAP_1")
-		firmware_page.asserting_version_unavailable_img()
-	
-	def test_ath_8344_downgrade_iap_224_225_to_34_and_blow(self):
-		conf = self.config.config_vars
-		firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
-		firmware_page.upgrade_firmware_using_custom_build_option(conf.version_firmware,"IAP_1")
-		firmware_page.asserting_version_unavailable_img()
-		
+    def test_ath_6836_check_ui_stability_after_page_reload(self):
+        self._view_monitoring_pages()
+        self.browser.refresh()
+        firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
+        firmware_page.isPageLoaded()    
+        self.logout_and_login_back()
+        firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
+        firmware_page.isPageLoaded()  
+    
+    def test_ath_8343_downgrade_iap_224_225_to_34_and_blow(self):
+        conf = self.config.config_vars
+        firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
+        firmware_page.upgrade_firmware_using_custom_build_option(conf.version_firmware,"IAP_1")
+        firmware_page.asserting_version_unavailable_img()
+    
+    def test_ath_4426_downgrade_iap_224_225_to_34_and_blow(self):
+        conf = self.config.config_vars
+        firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
+        firmware_page.upgrade_firmware_using_custom_build_option(conf.invalid_version_firmware,"IAP_1")
+        firmware_page.asserting_version_unavailable_img()
+    
+    def test_ath_8344_downgrade_iap_224_225_to_34_and_blow(self):
+        conf = self.config.config_vars
+        firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
+        firmware_page.upgrade_firmware_using_custom_build_option(conf.version_firmware,"IAP_1")
+        firmware_page.asserting_version_unavailable_img()
+        
--- athena_automation/athenataf/tests/firmware_management/scheduled_upgrade/ScheduledUpgrade.py
+++ athena_automation/athenataf/tests/firmware_management/scheduled_upgrade/ScheduledUpgrade.py
@@ -14,32 +14,37 @@ class ScheduledUpgrade(AthenaGUITestCase):
         ''' manual...
             upgrade any swarm
         '''
-        conf=self.config.config_vars
-        inner_left_panel = self.TopPanel.click_slider_icon()
-        # self.take_s1_snapshot("show_version")
-        inner_left_panel.select_default_group()
+        conf = self.config.config_vars
         firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
-        firmware_page.upgrade_firmware_using_manual_option(conf.version_type_value_2,conf.version_firmware_3)
-        firmware_page.click_post_firmware_upgrade()
-        DeviceLibrary.connect_device_to_server('IAP_1')
-        # DeviceLibrary.connect_device_to_server('IAP_2')
-        time.sleep(60)
-        firmware_page.assert_firmware_version('IAP_1',conf.firmware_upgrade_version)
-        # firmware_page.assert_firmware_version('IAP_2',conf.firmware_upgrade_version)
-        # self.take_s2_snapshot("show_version")
-        # firmware_page.buy_time()
-        # firmware_page.asserting_device_upgraded_successful1()
-        firmware_page.select_first_vc()
+        # self.take_s1_snapshot("show_version")
+        firmware_page.select_vc_for_upgrade('IAP_1')
+        firmware_page.select_vc_for_upgrade('IAP_2')
         firmware_page.clicking_on_upgrade_firmware()
-        firmware_page.setting_firmware_upgrade_manual_option(conf.version_type_value_2,conf.firmware_base_version)
+        firmware_page.setting_firmware_upgrade_manual_option(option=conf.version_type_value_2,version=conf.firmware_upgrade_version)
         firmware_page.click_post_firmware_upgrade()
-        time.sleep(60)
-        DeviceLibrary.connect_device_to_server('IAP_1')
-        # DeviceLibrary.connect_device_to_server('IAP_2')
-    
-        self.buy_time()
+        firmware_page.buy_time()
+        time.sleep(5)
+        DeviceLibrary.getPrompt("IAP_1")
+        DeviceLibrary.connect_device_to_server("IAP_1")
+        DeviceLibrary.getPrompt("IAP_2")
+        DeviceLibrary.connect_device_to_server("IAP_2")
+        time.sleep(5)
         firmware_page.assert_firmware_version('IAP_1',conf.firmware_upgrade_version)
-        # firmware_page.assert_firmware_version('IAP_2',conf.firmware_upgrade_version)
+        firmware_page.assert_firmware_version('IAP_2',conf.firmware_upgrade_version)
+        # self.take_s2_snapshot("show_version")
+        '''
+        downgrade back to base version
+        '''
+        firmware_page.select_vc_for_upgrade('IAP_1')
+        firmware_page.select_vc_for_upgrade('IAP_2')
+        firmware_page.clicking_on_upgrade_firmware()
+        firmware_page.setting_firmware_upgrade_manual_option(option=conf.version_type_value_2,version=conf.firmware_base_version)
+        firmware_page.click_post_firmware_upgrade()
+        firmware_page.buy_time()
+        DeviceLibrary.getPrompt("IAP_1")
+        DeviceLibrary.connect_device_to_server("IAP_1")
+        DeviceLibrary.getPrompt("IAP_2")
+        DeviceLibrary.connect_device_to_server("IAP_2")
         
     def test_ath_4411_upgrade_multiple_vcs_together(self):
         '''automatic '''
@@ -88,35 +93,35 @@ class ScheduledUpgrade(AthenaGUITestCase):
         firmware_page.assert_cancel_upgrade_button()
         
         
-	def test_ath_9058_immediate_upgrade_beta_image(self):
-		''' 
-		Immediate -- manual upgrade
-		'''
-		conf = self.config.config_vars
-		firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
-		firmware_page.select_vc_for_upgrade('IAP_1')
-		firmware_page.clicking_on_upgrade_firmware()
-		firmware_page.setting_firmware_upgrade_manual_option(option=conf.version_type_value_2,version=conf.firmware_upgrade_version)
+    def test_ath_9058_immediate_upgrade_beta_image(self):
+        ''' 
+        Immediate -- manual upgrade
+        '''
+        conf = self.config.config_vars
+        firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
+        firmware_page.select_vc_for_upgrade('IAP_1')
+        firmware_page.clicking_on_upgrade_firmware()
+        firmware_page.setting_firmware_upgrade_manual_option(option=conf.version_type_value_2,version=conf.firmware_upgrade_version)
 
-		firmware_page.select_later_date_radio()
-		firmware_page.set_upgrade_after_ten_mins()
-		firmware_page.click_post_firmware_upgrade()
+        firmware_page.select_later_date_radio()
+        firmware_page.set_upgrade_after_ten_mins()
+        firmware_page.click_post_firmware_upgrade()
 
-		time.sleep(1200)
-		DeviceLibrary.getPrompt("IAP_1")
-		DeviceLibrary.connect_device_to_server('IAP_1')
-		time.sleep(60)
-		firmware_page.assert_firmware_version('IAP_1',conf.firmware_upgrade_version) 
+        time.sleep(1200)
+        DeviceLibrary.getPrompt("IAP_1")
+        DeviceLibrary.connect_device_to_server('IAP_1')
+        time.sleep(60)
+        firmware_page.assert_firmware_version('IAP_1',conf.firmware_upgrade_version) 
 
-		firmware_page.select_vc_for_upgrade('IAP_1')
-		firmware_page.clicking_on_upgrade_firmware()
-		firmware_page.setting_firmware_upgrade_manual_option(conf.version_type_value_2,conf.firmware_base_version)
-		firmware_page.click_post_firmware_upgrade()
-		time.sleep(60)
-		DeviceLibrary.getPrompt("IAP_1")
-		DeviceLibrary.connect_device_to_server('IAP_1')
-		self.buy_time()
-		firmware_page.assert_firmware_version('IAP_1',conf.firmware_upgrade_version) 
+        firmware_page.select_vc_for_upgrade('IAP_1')
+        firmware_page.clicking_on_upgrade_firmware()
+        firmware_page.setting_firmware_upgrade_manual_option(conf.version_type_value_2,conf.firmware_base_version)
+        firmware_page.click_post_firmware_upgrade()
+        time.sleep(60)
+        DeviceLibrary.getPrompt("IAP_1")
+        DeviceLibrary.connect_device_to_server('IAP_1')
+        self.buy_time()
+        firmware_page.assert_firmware_version('IAP_1',conf.firmware_upgrade_version) 
         
     def test_ath_4408_scheduled_upgrade_automatic(self):
         ''' automatic upgrade '''
@@ -154,9 +159,8 @@ class ScheduledUpgrade(AthenaGUITestCase):
         '''manual upgrade and downgrade of a swarm'''
         conf = self.config.config_vars
         firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
-        # self.take_s1_snapshot("show_version")
         firmware_page.select_vc_for_upgrade('IAP_1')
-        # firmware_page.select_vc_for_upgrade('IAP_2')
+        firmware_page.select_vc_for_upgrade('IAP_2')
         firmware_page.clicking_on_upgrade_firmware()
         firmware_page.setting_firmware_upgrade_manual_option(conf.version_type_value_2,conf.firmware_upgrade_version)
         firmware_page.select_later_date_radio()
@@ -167,14 +171,14 @@ class ScheduledUpgrade(AthenaGUITestCase):
         time.sleep(1200)
         DeviceLibrary.getPrompt("IAP_1")
         DeviceLibrary.connect_device_to_server('IAP_1')
-        # DeviceLibrary.getPrompt("IAP_1")
-        # DeviceLibrary.connect_device_to_server('IAP_2')
+        DeviceLibrary.getPrompt("IAP_1")
+        DeviceLibrary.connect_device_to_server('IAP_2')
         firmware_page.buy_time()
         firmware_page.assert_firmware_version('IAP_1',conf.firmware_upgrade_version) 
-        # firmware_page.assert_firmware_version('IAP_2',conf.firmware_upgrade_version)
+        firmware_page.assert_firmware_version('IAP_2',conf.firmware_upgrade_version)
         ''' now downgrading'''
         firmware_page.select_vc_for_upgrade('IAP_1')
-        # firmware_page.select_vc_for_upgrade('IAP_2')
+        firmware_page.select_vc_for_upgrade('IAP_2')
         firmware_page.clicking_on_upgrade_firmware()
         firmware_page.setting_firmware_upgrade_manual_option(conf.version_type_value_2,conf.firmware_base_version)
         firmware_page.select_later_date_radio()
@@ -184,8 +188,7 @@ class ScheduledUpgrade(AthenaGUITestCase):
         firmware_page.asser_successfully_upgrading_msg()
         time.sleep(1200)
         DeviceLibrary.connect_device_to_server('IAP_1')
-        # DeviceLibrary.connect_device_to_server('IAP_2')
+        DeviceLibrary.connect_device_to_server('IAP_2')
         firmware_page.buy_time()
         firmware_page.assert_firmware_version('IAP_1',conf.firmware_upgrade_version) 
-        # firmware_page.assert_firmware_version('IAP_2',conf.firmware_upgrade_version)
-        # self.take_s2_snapshot("show_version")
+        firmware_page.assert_firmware_version('IAP_2',conf.firmware_upgrade_version)
