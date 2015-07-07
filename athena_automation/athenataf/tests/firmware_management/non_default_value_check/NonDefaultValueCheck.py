0bd326051fd572ca8f96d2371052c6b54a558c7e
raju-tm <raju_set@testmile.com>
2015-07-07 16:10:16 +0530

    TestClass : Adding new testclass NonDefaultValueCheck.py and new testcases

---
+++ athena_automation/athenataf/tests/firmware_management/non_default_value_check/NonDefaultValueCheck.py
@@ -0,0 +1,41 @@
+import logging
+logger = logging.getLogger('athenataf')
+import time
+
+from athenataf.lib.functionality.test.AthenaGUITestCase import AthenaGUITestCase
+from athenataf.lib.functionality.common import DeviceLibrary
+
+class NonDefaultValueCheck(AthenaGUITestCase):
+    '''
+    Test class for Firmware Management Non DefaultValueCheck.
+    '''
+    
+    def test_ath_11846_firmware_upgrade_in_vc_automatic(self):
+        conf = self.config.config_vars
+        firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
+        latest_version = firmware_page.get_latest_recommended_version()
+        firmware_page.select_vc_for_upgrade('IAP_1')
+        firmware_page.upgrade_button.click()
+        # firmware_page.select_later_date_radio()
+        # firmware_page.set_upgrade_after_ten_mins()
+        firmware_page.click_post_firmware_upgrade()
+        time.sleep(1000)
+        # self.buy_time()
+        DeviceLibrary.getPrompt("IAP_1")
+        DeviceLibrary.connect_device_to_server('IAP_1')
+        # self.buy_time()
+        firmware_page.assert_firmware_version('IAP_1',latest_version)
+        
+    def test_ath_11941_firmware_upgrade_in_vc_manual(self):
+        conf = self.config.config_vars
+        firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
+        firmware_page.select_vc_for_upgrade('IAP_1')        
+        firmware_page.clicking_on_upgrade_firmware()
+        firmware_page.setting_firmware_upgrade_manual_option(conf.version_type_value_2,conf.firmware_version_4122)
+        firmware_page.click_post_firmware_upgrade()
+        time.sleep(1500)
+        # self.buy_time()       
+        DeviceLibrary.getPrompt("IAP_1")
+        DeviceLibrary.connect_device_to_server('IAP_1')
+        # self.buy_time()
+        firmware_page.assert_firmware_version('IAP_1',conf.firmware_version_4122)
