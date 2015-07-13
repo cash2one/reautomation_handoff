ee74500d1d36366a422b67ec89abc0545898d905
raju-tm <raju_set@testmile.com>
2015-07-13 13:16:24 +0530

    Devices.py : removing duplicate class and adding new fields in IAP and switchdetails

--- athena_automation/athenataf/config/devices.py
+++ athena_automation/athenataf/config/devices.py
@@ -3,136 +3,64 @@ CONNECTION_WAIT_TIME = 10
 
 
 class IAP_1:
-   '''
-   Device under test details .
-   '''
-   consoleIp = "10.15.140.8"
-   consoleUsername = "root"
-   consolePassword = "Aruba@123"
-   ip = "10.15.141.54"
-   username = "admin"
-   password = "test123"
-   type = "IAP"
-   version = '4.1.1.2'
-   server = "jenkins-DeployDemo-SNG-411.test.SNG1.arubathena.com"
-   protocol = "ssh"
-   port = "7011"
-   ap_type = "AP-225"
-   mac = "24:de:c6:cb:76:cc"
-   location = "Chennai"
-   vc_name = 'Instant-CB:76:CC'
-   prompt = "#"
+	'''
+	Device under test details .
+	'''
+	consoleIp = "10.15.140.8"
+	consoleUsername = "root"
+	consolePassword = "Aruba@123"
+	ip = "10.15.141.54"
+	username = "admin"
+	password = "test123"
+	type = "IAP"
+	version = '4.1.1.2'
+	server = "jenkins-DeployDemo-SNG-411.test.SNG1.arubathena.com"
+	protocol = "ssh"
+	port = "7011"
+	ap_type = "AP-225"
+	mac = "24:de:c6:cb:76:cc"
+	mac_address = "24:de:c6:cb:1f:70"
+	gatewayip= "10.15.141.33"
+	netmask = "255.255.255.248"
+	gateway_ip= "10.15.141.33"
+	dns_ip= "8.8.8.8"
+	location = "Chennai"
+	vc_name = 'Instant-CB:76:CC'
+	prompt = "#"
+	serial_no = 'CT0394163'
+	activation_id = 'NU38BOK5'  
 
 
 
-# class IAP_2:
-#    '''
-#    Device under test details .
-#    '''
-#    consoleIp = "10.29.2.53"
-#    consoleUsername = "console"
-#    consolePassword = "console"
-#    ip = "10.29.27.19"
-#    username = "admin"
-#    password = "test123"
-#    type = "IAP"
-#    version = '4.1.2.1'
-#    server = "jenkins-DeployDemo-SNG-406.test.SNG1.arubathena.com"
-#    protocol = "ssh"
-#    port = "7009"
-#    ap_type = "AP-224"
-#    mac = "18:64:72:c9:a2:f4"
-#    location = "Chennai"
-#    prompt = "#"
-#
-# class Client_1:
-#    ip = "10.29.27.21"
-#    eth_ip = "10.29.254.20"
-#    username = "athenatest"
-#    password = "Test1234"
-#    type = "CLIENT"
-#    os = "Win8"
-#    protocol = "ssh"
-#    mac = "c4:d9:87:00:f5:32"
-#    location = "Chennai"
-##
-# class IAP_1:
-#      '''
-#      Device under test details .
-#      '''
-#      consoleIp = "10.29.2.53"
-#      consoleUsername = "console"
-#      consolePassword = "console"
-#      ip = "10.29.27.27"
-#      username = "admin"
-#      password = "test123"
-#      type = "IAP"
-#      version = '4.0'
-#      server = "jenkins-DeployDemo-SNG-403.test.SNG1.arubathena.com"
-#      protocol = "ssh"
-#      port = "7012"
-#      ap_type = "AP-225"
-#      mac = "18:64:72:cc:b8:f4"
-#      location = "Chennai"
-#      prompt = "#"
+class Client_1:
+	ip = "10.29.27.21"
+	eth_ip = "10.29.254.20"
+	username = "athenatest"
+	password = "Test1234"
+	type = "CLIENT"
+	os = "Win8"
+	protocol = "ssh"
+	mac = "c4:d9:87:00:f5:32"
+	location = "Chennai"
 
 
-# class IAP_2:
-#     '''
-#     Device under test details .
-#     '''
-#     consoleIp = "10.29.2.53"
-#     consoleUsername = "console"
-#     consolePassword = "console"
-#     ip = "10.29.27.26"
-#     username = "admin"
-#     password = "test123"
-#     type = "IAP"
-#     version = '4.0'
-#     server = "jenkins-DeployDemo-SNG-403.test.SNG1.arubathena.com"
-#     protocol = "ssh"
-#     port = "7011"
-#     ap_type = "AP-224"
-#     mac = "18:64:72:c9:a3:0a"
-#     location = "Chennai"
-#     prompt = "#"
+class Switch_1:
+	consoleIp = "10.15.148.253"
+	consoleUsername = "admin"
+	consolePassword = "aruba123"
+	ip = "10.15.148.38"
+	username = "admin"
+	password = "admin123"
+	enable = "enable"
+	type = "SWITCH"
+	version = 4.0
+	server = "54.179.64.28"
+	protocol = "telnet"
+	port = "7004"
+	prompt = ") #"
+	switch_name = "ArubaS2500"
+	location = "Bangalore, Karnataka, India"
+	mac = '00:0b:86:a4:3a:80'
+	serial = "BY0007437"
+	activation_id = 'WHB7HTO3'
 
-# class Switch_1:
-    # consoleIp = "10.15.148.253"
-    # consoleUsername = "admin"
-    # consolePassword = "aruba123"
-    # ip = "10.15.148.38"
-    # username = "admin"
-    # password = "admin123"
-    # enable = "enable"
-    # type = "SWITCH"
-    # version = 4.0
-    # server = "54.179.64.28"
-    # protocol = "telnet"
-    # port = "7004"
-    # prompt = ") #"
-    # switch_name = "ArubaS2500"
-    # location = "Bangalore, Karnataka, India"
-    # mac = '00:0b:86:a4:3a:80'
-    # serial = "BY0007437"
-    # activation_id = 'WHB7HTO3'
-	
-# class Switch_1:
-    # consoleIp = "10.15.148.253"
-    # consoleUsername = "admin"
-    # consolePassword = "aruba123"
-    # ip = "10.15.148.78"
-    # username = "admin"
-    # password = "test123"
-    # enable = "enable"
-    # type = "SWITCH"
-    # version = 4.0
-    # server = "54.179.64.28"
-    # protocol = "telnet"
-    # port = "7010"
-    # prompt = ") #"
-    # switch_name = "ArubaS1500"
-    # location = "Bangalore, Karnataka, India"
-    # mac = '00:0b:86:d1:2b:c0'
-    # serial = "CD0005611"
-    # activation_id = 'A4BBAAUE'
