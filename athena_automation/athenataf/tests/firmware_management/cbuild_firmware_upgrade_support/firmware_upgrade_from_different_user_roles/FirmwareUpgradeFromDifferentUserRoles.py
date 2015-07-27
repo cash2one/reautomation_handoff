1f7a30aaa43891e42aa0508f54d87a632de1468b
raju-tm <raju_set@testmile.com>
2015-07-27 16:59:06 +0530

    TestClass : Updating test cases in FirmwareUpgradeFromDifferentUserRoles.py

--- athena_automation/athenataf/tests/firmware_management/cbuild_firmware_upgrade_support/firmware_upgrade_from_different_user_roles/FirmwareUpgradeFromDifferentUserRoles.py
+++ athena_automation/athenataf/tests/firmware_management/cbuild_firmware_upgrade_support/firmware_upgrade_from_different_user_roles/FirmwareUpgradeFromDifferentUserRoles.py
@@ -3,6 +3,7 @@ logger = logging.getLogger('athenataf')
 from athenataf.lib.functionality.test.AthenaGUITestCase import AthenaGUITestCase
 from athenataf.lib.functionality.common.deviceverifier import DeviceVerifier
 import time
+import traceback
 
 class FirmwareUpgradeFromDifferentUserRoles(AthenaGUITestCase):
     '''
@@ -14,31 +15,49 @@ class FirmwareUpgradeFromDifferentUserRoles(AthenaGUITestCase):
         user_management_page=self.LeftPanel.go_to_user_management()
         user_management_page.delete_if_any_user_present()
         user_management_page.create_new_user(conf.email_read_only,conf.user_setting_group_value,conf.user_access_level_read_only)
-        device_management_page = self.LeftPanel.go_to_device_management()
+        # device_management_page = self.LeftPanel.go_to_device_management()
         #device_management_page.search_device_using_mac_address()
         #device_management_page.change_device1_to_unassigned()
         self.logout()
+        time.sleep(5)
         self.login(access_level = 'read_only')
+        time.sleep(5)
         firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
         #firmware_page.upgrade_firmware_using_custom_build_option(conf.version_type_value_2)
-        firmware_page.upgrade_firmware_using_custom_build_option(conf.cbuild_version)
+        firmware_page.upgrade_firmware_using_custom_build_option(conf.c_builb_image_version,'IAP_1')
         firmware_page.asserting_upgrade_failure_error()
+        self.logout()
+        time.sleep(5)
+        self.login()
+        user_management_page=self.LeftPanel.go_to_user_management()
+        user_management_page.delete_if_any_user_present()
         
+      
     def test_ath_9177_verify_read_write_user_has_no_permission_to_upgrade(self):
         conf = self.config.config_vars
 
         user_management_page=self.LeftPanel.go_to_user_management()
         user_management_page.delete_if_any_user_present()
         user_management_page.create_new_user(conf.email_read_write,conf.user_setting_group_value,conf.user_access_level_read_write)
-        device_management_page = self.LeftPanel.go_to_device_management()
+        # device_management_page = self.LeftPanel.go_to_device_management()
         #device_management_page.search_device_using_mac_laddress()
         #device_management_page.change_device1_to_unassigned()
         self.logout()
+        time.sleep(5)
         self.login(access_level = 'read_write')
+        time.sleep(5)
         firmware_page = self.LeftPanel.go_to_maintenance_Firmware_page()
-        firmware_page.upgrade_firmware_using_custom_build_option(conf.version_firmware_3)
-        firmware_page.asserting_upgrade_button()
-        firmware_page.asserting_upgrade_button_disabled()
+        firmware_page.upgrade_firmware_using_custom_build_option(conf.version_firmware_3,'IAP_1')
+        # firmware_page.asserting_upgrade_button()
+        if not firmware_page.disable_upgrade_button:
+            raise AssertionError("FirmWarePage : 'Upgrade' button does not enabled ")
+        # firmware_page.asserting_upgrade_button_disabled()
+        firmware_page.cancel_firmware_upgrade()
+        self.logout()
+        time.sleep(5)
+        self.login()
+        user_management_page=self.LeftPanel.go_to_user_management()
+        user_management_page.delete_if_any_user_present()
 
     def test_ath_9176_verify_admin_user_has_permission_to_upgrade_firmware(self):
         conf = self.config.config_vars
